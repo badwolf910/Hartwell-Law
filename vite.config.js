3ba8ebdf080d7a1972226e0842d2cfc85ed2a1d0
Danielle Smith <dns22668@yahoo.com>
2026-09-23 18:34:47 -0400

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/Hartwell-Law/',
   plugins: [react()],
   server: {
     host: '0.0.0.0',
